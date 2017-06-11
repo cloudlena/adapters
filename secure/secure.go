95e3cc072f48b08a77cdc6e09aa9f14c5be2d524
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2017-06-11 22:43:35 +0200

    Add secure handler

---
+++ secure/secure.go
@@ -0,0 +1,21 @@
+package secure
+
+import (
+	"net/http"
+
+	"github.com/mastertinner/adapters"
+)
+
+// Handler redirects any HTTP request to HTTPS.
+func Handler(enabled bool) adapters.Adapter {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if enabled && r.TLS == nil {
+				url := "https://" + r.Host + r.URL.String()
+				http.Redirect(w, r, url, http.StatusPermanentRedirect)
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
