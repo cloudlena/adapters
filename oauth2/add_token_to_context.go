646e473ed24514a5d3903fd7c21560efa12db697
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2017-09-07 22:11:11 +0200

    Switch from Redis to using JWTs

---
+++ oauth2/add_token_to_context.go
@@ -0,0 +1,41 @@
+package oauth2
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/mastertinner/adapters"
+)
+
+// AddTokenToContext checks if a request is authenticated through OAuth2 and JWT.
+// If it is, it will add the token to the request's context.
+func AddTokenToContext(sessionSecret string, tokenContextKey interface{}) adapters.Adapter {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			authHeader := r.Header.Get("Authorization")
+			tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+			token, err := jwt.Parse(tokenString, func(tok *jwt.Token) (interface{}, error) {
+				if _, ok := tok.Method.(*jwt.SigningMethodHMAC); !ok {
+					err := fmt.Errorf("Unexpected signing method: %v", tok.Header["alg"])
+					return nil, err
+				}
+				return []byte(sessionSecret), nil
+			})
+			if err != nil || !token.Valid {
+				status := http.StatusUnauthorized
+				http.Error(w, http.StatusText(status), status)
+				return
+			}
+
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, tokenContextKey, token)
+			r = r.WithContext(ctx)
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
