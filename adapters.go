329015007468293916b6778f28184e0406435f55
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2017-09-13 00:14:31 +0200

    Reverse adapter application order

--- adapters.go
+++ adapters.go
@@ -8,8 +8,8 @@ type Adapter func(http.Handler) http.Handler
 
 // Adapt adds adapters to an HTTP handler.
 func Adapt(h http.Handler, adapters ...Adapter) http.Handler {
-	for i := len(adapters) - 1; i >= 0; i-- {
-		h = adapters[i](h)
+	for _, a := range adapters {
+		h = a(h)
 	}
 
 	return h
