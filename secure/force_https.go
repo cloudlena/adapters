ce5e21a059d1df739a2a6c4e1e42cf89ff508e5c
Lena Fuhrimann <6780471+cloudlena@users.noreply.github.com>
2017-11-09 09:11:34 +0100

    Add BasicAuth handler

--- secure/force_https.go
+++ secure/force_https.go
@@ -6,8 +6,8 @@ import (
 	"github.com/mastertinner/adapters"
 )
 
-// Handler redirects any HTTP request to HTTPS.
-func Handler(enabled bool) adapters.Adapter {
+// ForceHTTPS redirects any HTTP request to HTTPS.
+func ForceHTTPS(enabled bool) adapters.Adapter {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			notSecure := r.TLS == nil && r.Header.Get("X-Forwarded-Proto") != "https"
