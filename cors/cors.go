c1fd7f5ebe37f0db9f7f4f16dfabb1ad09b3d23d
Tobias Fuhrimann <tobias.fuhrimann@gmail.com>
2017-11-26 10:35:50 +0100

    Add CORS package

---
+++ cors/cors.go
@@ -0,0 +1,53 @@
+package cors
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/mastertinner/adapters"
+)
+
+// Options configure a CORS handler.
+type Options struct {
+	Origins string
+	Methods string
+	Headers string
+}
+
+// Handler logs HTTP requests.
+func Handler(o Options) adapters.Adapter {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if o.Origins == "" {
+				o.Origins = "*"
+			}
+			if o.Methods == "" {
+				o.Methods = strings.Join([]string{
+					http.MethodPost,
+					http.MethodGet,
+					http.MethodPut,
+					http.MethodPatch,
+					http.MethodDelete,
+					http.MethodHead,
+				}, ", ")
+			}
+			if o.Headers == "" {
+				o.Headers = r.Header.Get("Access-Control-Request-Headers")
+			}
+
+			if r.Header.Get("Origin") != "" {
+				w.Header().Set("Access-Control-Allow-Origin", o.Origins)
+				w.Header().Set("Access-Control-Allow-Methods", o.Methods)
+				if o.Headers != "" {
+					w.Header().Set("Access-Control-Allow-Headers", o.Headers)
+				}
+			}
+
+			if r.Method == http.MethodOptions {
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
+}
