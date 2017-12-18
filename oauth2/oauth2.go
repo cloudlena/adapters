2dea9e70f14411646113c2437deab7c1d6c548d9
Tobias Fuhrimann <tobias.fuhrimann@gmail.com>
2017-12-18 22:25:16 +0100

    Add TokenLoginHandler for existing tokens

--- oauth2/oauth2.go
+++ oauth2/oauth2.go
@@ -67,3 +67,28 @@ func CallbackHandler(config *oa2.Config, sessionSecret string, tokenTTL time.Dur
 		issueSession(w, r, claims, tokenTTL, sessionSecret, redirectURI)
 	})
 }
+
+// TokenLoginHandler logs a user who already has an access token in.
+func TokenLoginHandler(config *oa2.Config, sessionSecret string, tokenTTL time.Duration, parseTok ParseTokenFunc) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			status := http.StatusBadRequest
+			http.Error(w, http.StatusText(status), status)
+			return
+		}
+
+		accessToken := r.FormValue("access_token")
+		tok := &oa2.Token{AccessToken: accessToken}
+
+		claims, err := parseTok(tok)
+		if err != nil {
+			fmt.Println("error parsing token:", err)
+			status := http.StatusUnauthorized
+			http.Error(w, http.StatusText(status), status)
+			return
+		}
+
+		issueSession(w, r, claims, tokenTTL, sessionSecret, "")
+	})
+}
